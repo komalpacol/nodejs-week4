ff27ce4910e2b6c2c4fd3b52c8182b14495f3ef4
komalpacol <komalshr2005@gmail.com>
2025-07-11 11:37:02 +0530

    imported chalk npm

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+console.log(chalk.blue('hello world'));
+console.log(chalk.red.bold('this is an error message'));
+console.log(chalk.green.underline('importing chalk module'));
